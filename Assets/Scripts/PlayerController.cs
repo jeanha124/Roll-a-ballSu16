95f699eb6864421e74034013d00ea6384720a032
Jean Woo Ha <jeanha124@gmail.com>
2016-06-02 14:22:46 -0400

    Collecting the Pick Up Objects
    had some problems with that Pick Up portion, but was able to work once
    I put it as PickUp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
 
 	public float speed;
 
 	private Rigidbody rb;
 
-	void Start (){
+	void Start ()
+	{
 		rb = GetComponent<Rigidbody> ();
 	}
 
-	void FixedUpdate () {
+	void FixedUpdate () 
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 			
 		rb.AddForce (movement * speed); 
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+		}
 	}
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotate : MonoBehaviour {
+public class Rotate : MonoBehaviour 
+{
 
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
